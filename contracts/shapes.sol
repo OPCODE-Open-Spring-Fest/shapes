f0e7e2b48f955c223b80386c6ad22e03ef11b702
x256 <153709773+xTwo56@users.noreply.github.com>
2024-03-29 15:16:58 +0530

    feat: created function to find interior angle of a shape

--- contracts/shapes.sol
+++ contracts/shapes.sol
@@ -5,6 +5,7 @@ contract shapes{
     // If you want to compare two strings simply use the function compareStrings() which is already created which returns bool 
 
     mapping(string => uint ) shape_number;
+    mapping(uint => uint) interior_angle;
     string [] name=["point","line","triangle","quadrilateral","pentagon","hexagon","heptagon","octagon","nonagon","decagon"];
     
     
@@ -19,6 +20,15 @@ contract shapes{
          shape_number["octagon"]=8;
          shape_number["nonagon"]=9;
          shape_number["decagon"]=10;
+
+        interior_angle[3]=60;
+        interior_angle[4]=90;
+        interior_angle[5]=108;
+        interior_angle[6]=120;
+        interior_angle[7]=128;
+        interior_angle[8]=135;
+        interior_angle[9]=140;
+        interior_angle[10]=144;
     }
 
 
@@ -54,6 +64,12 @@ contract shapes{
         require(sides>0 && sides<11, "Input should lie in (0, 10]");
         _;
     }
+    
+    modifier sidesLimitAngle(uint sides){
+        require(sides>2 && sides<11, "Input should lie in (2, 10]");
+        _;
+    }
+    
     modifier shapeChecker(string memory shape){
         bool isPresent = (shape_number[shape] == 0)? false:true;
         require(isPresent, "Input valid shape with 10 or less sides");
@@ -90,4 +106,9 @@ contract shapes{
     function equal(uint sides, string memory shape) public view sidesLimit(sides) shapeChecker(shape) returns (bool){
         return (sides == shape_number[shape])? true:false;
     }
+     function interiorAngle(uint sides) public view sidesLimitAngle(sides) returns(uint){
+        return interior_angle[sides];
+    }
+       
 }
+
